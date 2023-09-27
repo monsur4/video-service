072e25a3e1de5435903cfa409e981b0cd6d94441
Monsuru <okuniyimonsuru@yahoo.com>
2023-09-27 08:03:39 +0100

    update videoRepository

--- src/main/java/com/mon/videoservice/repository/VideoRepository.java
+++ src/main/java/com/mon/videoservice/repository/VideoRepository.java
@@ -2,8 +2,17 @@ package com.mon.videoservice.repository;
 
 import com.mon.videoservice.model.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
+    Video findByName(String name);
+
+    boolean existsByName(String name);
+
+    @Query(nativeQuery = true, value = "SELECT name FROM video ORDER BY name")
+    List<String> fetchAllVideoNames();
 }
