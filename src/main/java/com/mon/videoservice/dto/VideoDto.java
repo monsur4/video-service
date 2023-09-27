3197fb0d31def90b8c6ffb736b02ed66de20fa18
Monsuru <okuniyimonsuru@yahoo.com>
2023-09-27 07:57:51 +0100

    update videoDto

--- src/main/java/com/mon/videoservice/dto/VideoDto.java
+++ src/main/java/com/mon/videoservice/dto/VideoDto.java
@@ -1,16 +1,16 @@
 package com.mon.videoservice.dto;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
-@Builder
 public class VideoDto {
     private String name;
 
     private String[] tags;
 
-    private byte[] data;
+    private MultipartFile videoFile;
 }
