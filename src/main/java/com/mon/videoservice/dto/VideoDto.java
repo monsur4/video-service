1ed2a73434a57c4d065894599bc9f2069a834318
Monsuru <okuniyimonsuru@yahoo.com>
2023-09-29 20:42:11 +0100

    add data field to videoDto

--- src/main/java/com/mon/videoservice/dto/VideoDto.java
+++ src/main/java/com/mon/videoservice/dto/VideoDto.java
@@ -1,16 +1,22 @@
 package com.mon.videoservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
 @NoArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class VideoDto {
     private String name;
 
     private String[] tags;
 
     private MultipartFile videoFile;
+
+    private byte[] data;
 }
