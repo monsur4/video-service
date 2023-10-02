e209862d9704690fc0ea3e3ad9f75a626ac76868
Monsuru <okuniyimonsuru@yahoo.com>
2023-10-02 21:35:52 +0100

    update-fetch method -append filename with .mp4- specify return type as octet-stream

--- src/main/java/com/mon/videoservice/controller/VideoController.java
+++ src/main/java/com/mon/videoservice/controller/VideoController.java
@@ -4,6 +4,12 @@ import com.mon.videoservice.dto.ResponseDto;
 import com.mon.videoservice.dto.VideoDto;
 import com.mon.videoservice.service.VideoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,10 +31,12 @@ public class VideoController {
     }
 
     @GetMapping("fetch")
-    public ResponseDto<VideoDto> fetch(@RequestParam("name") String name){
-        return ResponseDto.wrapSuccessResponse(
-                videoService.fetchVideo(name)
-        );
+    public ResponseEntity<Resource> fetch(@RequestParam("name") String name){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + name +".mp4")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(new ByteArrayResource(videoService.fetchVideo(name).getData()));
     }
 
     @PostMapping("save")
